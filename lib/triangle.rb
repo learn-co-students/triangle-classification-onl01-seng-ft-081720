444af8ca18ee38e660d7c62d254f4d13e44cb713
atBeatty <adam.t.beatt@gmail.com>
2020-09-03 14:09:13 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,60 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+@@all = []
+  def initialize(side1, side2, side3)
+
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    
+    @@all << self
+
+  end
+
+  def kind
+    sides = [@side1, @side2, @side3]
+    # "NEGATIVE SIDE" if sides.any? {|side| side < 0}
+
+
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
+      begin
+        raise TriangleError
+        rescue Triangle => error
+        puts error.message
+      end
+    elsif (@side1 <= (@side2 - @side3).abs) || (@side2 <= (@side3 - @side1).abs) || (@side3 <= (@side2 - @side1).abs)
+      begin
+        raise TriangleError
+        rescue Triangle => error
+        puts error.message
+      end
+    end
+
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 != @side2 && @side2 != @side3 && @side3 != @side1
+      :scalene
+    else
+      :isosceles
+    end
+
+
+      
+
+    # begin
+    # raise TriangleError
+    # rescue TriangleError
+    # end
+
+
+  end
+
+
+class TriangleError < StandardError
+  def message
+    "One of your sides is negative."
+  end
+
+end
+
 end
