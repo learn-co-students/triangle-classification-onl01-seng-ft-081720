223490c6af2a392db9dc3ad39886f5850ff8d8a5
john glennan <john@Victorias-MacBook-Pro.local>
2020-09-06 17:26:15 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  if x == y && x == z && y == z
+     equilateral
+  elsif x != y && x != z && y != z
+     isosceles
+  else
+     scalene
+  end
   # write code here
 end
